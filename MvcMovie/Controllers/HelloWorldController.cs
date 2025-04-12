0106a618f67d2aabbce463a20d2e16e575afdb11
Pavan <74958980+PavanPamarthi118@users.noreply.github.com>
2025-04-12 22:39:21 +0530

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // Every public method in a controller is callable as an HTTP endpoint
+
+    // Controller methods: Are referred to as action methods.For example, the Index action method in the preceding code.
+    //Generally return an IActionResult or a class derived from ActionResult, not a type like string.
+
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // GET: /HelloWorld/Welcome/ 
+    // Requires using System.Text.Encodings.Web;
+    public string Welcome(string name, int numTimes = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+    }
+}
